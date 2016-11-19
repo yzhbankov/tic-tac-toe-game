0334d4d5ba323f2b0b6b04cf0c0a89410307c83d
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-11-19 20:27:04 +0200

    create simple 1 step algorithm

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
 /**
  * Created by Iaroslav Zhbankov on 14.11.2016.
  */
+(function () {
+    var arr = ["x", null, null, null, null, null, null, null, null];
+
+    function won(arr, type) {
+        for (var i = 0; i < 3; i++) {
+            if ((arr[0 + i * 3] == arr[1 + i * 3]) && (arr[0 + i * 3] == arr[2 + i * 3]) && (arr[0 + i * 3] == type)) {
+                return true;
+            }
+            if ((arr[i] == arr[i + 3]) && (arr[i + 3] == arr[i + 6]) && (arr[i] == type)) {
+                return true;
+            }
+            if ((arr[0] == arr[4]) && (arr[0] == arr[8]) && (arr[0] == type)) {
+                return true;
+            }
+            if ((arr[2] == arr[4]) && (arr[2] == arr[6]) && (arr[2] == type)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function minimax(arr, type) {
+        var newSet = [];
+        var result = false;
+        for (var i = 0; i < 9; i++) {
+            var brr = arr.slice();
+            if (brr[i] == null) {
+                brr[i] = type;
+                newSet.push(brr);
+            }
+        }
+        for (var j = 0; j < newSet.length; j++) {
+            if (won(newSet[j], type)) {
+                result = true;
+                return newSet[j];
+            }
+            if (!result) {
+                return newSet[Math.floor(Math.random() * newSet.length)];
+            }
+        }
+    }
+
+    minimax(arr);
+
+})();
